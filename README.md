f3f5825477390bb1b0637f3e515236a1414613b9
Michael Leaney <leahcimic@gmail.com>
2019-02-13 13:27:36 +0800

    Initial 0.0.1

---
+++ README.md
@@ -0,0 +1,41 @@
+# Karma summary reporter [![Build Status](https://travis-ci.org/leahciMic/karma-results-summary-reporter.svg?branch=master)](https://travis-ci.org/leahciMic/karma-results-summary-reporter)
+
+> A Karma plugin. Report current spec on browser crash/ disconnect. Provides full logs for each failure. Supports Travis folding.
+
+## Table of Contents
+
+- [Install](#install)
+- [Usage](#usage)
+- [Contribute](#contribute)
+- [License](#license)
+
+## Install
+
+```sh
+npm i karma-results-summary-reporter
+
+# Or with Yarn
+yarn add karma-results-summary-reporter
+```
+
+## Usage
+
+```js
+import KarmaSummaryReporter from `karma-results-summary-reporter`;
+
+const karmaSummaryReporter = new KarmaSummaryReporter;
+
+// fill in the rest.
+```
+
+Laziness has prevented usage instructions from being documented.
+
+## Contributing
+
+See [the contribute file](CONTRIBUTING.md)!
+
+PRs accepted.
+
+## License
+
+[MIT © Michael Leaney](LICENSE)
---
+++ src/index.js
@@ -0,0 +1,106 @@
+var travisFold = require('travis-fold');
+
+var SpecReporter = function (baseReporterDecorator, config, emitter) {
+  function formatTest(test) {
+    return test.suite.join(' > ') + ': ' + test.description;
+  }
+  baseReporterDecorator(this);
+
+  var perBrowserInfo = {};
+  var failures = [];
+
+  this.onBrowserStart = function(browser) {
+    perBrowserInfo[browser.id] = {
+      currentTest: undefined,
+      logs: [],
+    };
+  };
+
+  this.onRunStarted = function(browser) {
+    perBrowserInfo[browser.id] = {
+      currentTest: undefined,
+      logs: [],
+    };
+    failures = [];
+  };
+
+  this.onSpecComplete = function(browser, specResult) {
+    if (specResult.success === false) {
+      failures.push({
+        browser: browser,
+        specResult: specResult,
+        logs: perBrowserInfo[browser.id].logs,
+      });
+    }
+    perBrowserInfo[browser.id] = {
+      currentTest: undefined,
+      logs: [],
+    };
+    perBrowserInfo[browser.id].logs = [];
+  };
+
+  emitter.on('browser_info', (browser, info) => {
+    if (info.current_spec) {
+      perBrowserInfo[browser.id] = {
+        currentTest: info.current_spec,
+        logs: [],
+      };
+    }
+  });
+
+  this.onBrowserError = function(browser, error) {
+    var browserInfo = perBrowserInfo[browser.id];
+
+    if (browserInfo === undefined) {
+      return
+    }
+
+    var currentTest = browserInfo.currentTest
+
+    if (currentTest === undefined) {
+      return;
+    }
+
+    this.writeCommonMsg('\n' + browser.name + ' Had error during execution of test: ' + formatTest(currentTest) + '\n');
+
+    this.writeCommonMsg('Captured the following logs \n');
+
+
+    var output = browserInfo.logs.map((log) => {
+      return log.type + ': ' + log.log;
+    }).join('\n');
+
+    this.writeCommonMsg('\n' + travisFold.wrap('output', output) + '\n');
+  }
+
+  this.onRunComplete = function() {
+    perBrowserInfo = {};
+    var output = [];
+    failures.forEach(function (failure) {
+      travisFold.pushStart(output, 'browser');
+      output.push('Logs for ' + failure.browser.name + ' ' + failure.specResult.fullName);
+      failure.logs.forEach(function (log) {
+        output.push(log.type + ': ' + log.log);
+      });
+      travisFold.pushEnd(output, 'browser');
+    });
+    this.writeCommonMsg('\n' + output.join('\n') + '\n');
+    failures = [];
+  }
+
+  this.onBrowserLog = function(browser, log, type) {
+    if (!perBrowserInfo[browser.id]) {
+      return;
+    }
+    perBrowserInfo[browser.id].logs.push({ type: type, log: log });
+    if (perBrowserInfo[browser.id].logs.length > 1000) {
+      perBrowserInfo[browser.id].logs.shift();
+    }
+  }
+};
+
+SpecReporter.$inject = ['baseReporterDecorator', 'config', 'emitter'];
+
+module.exports = {
+  'reporter:summary': ['type', SpecReporter]
+};
