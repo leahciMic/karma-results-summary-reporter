902a414a8aa0f530204955a9b04d2b9e58f62153
Michael Leaney <leahcimic@gmail.com>
2019-02-13 21:04:30 +0800

    0.0.2 - support colors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 var travisFold = require('travis-fold');
+var colors = require('colors');
 
-var SpecReporter = function (baseReporterDecorator, config, emitter) {
+var SpecReporter = function(baseReporterDecorator, config, emitter) {
+  function formatLog(type, log) {
+    var colorMap = {
+      log: 'white',
+      warn: 'yellow',
+      error: 'red',
+      info: 'blue',
+      debug: 'grey'
+    };
+    if (config.colors) {
+      console.log('should be a color', type, colorMap[type]);
+      return String(type + ': ')[colorMap[type]] + log;
+    } else {
+      return type + ': ' + log;
+    }
+  }
+
   function formatTest(test) {
     return test.suite.join(' > ') + ': ' + test.description;
   }
+
   baseReporterDecorator(this);
 
   var perBrowserInfo = {};
@@ -12,14 +30,14 @@ var SpecReporter = function (baseReporterDecorator, config, emitter) {
   this.onBrowserStart = function(browser) {
     perBrowserInfo[browser.id] = {
       currentTest: undefined,
-      logs: [],
+      logs: []
     };
   };
 
   this.onRunStarted = function(browser) {
     perBrowserInfo[browser.id] = {
       currentTest: undefined,
-      logs: [],
+      logs: []
     };
     failures = [];
   };
@@ -29,21 +47,21 @@ var SpecReporter = function (baseReporterDecorator, config, emitter) {
       failures.push({
         browser: browser,
         specResult: specResult,
-        logs: perBrowserInfo[browser.id].logs,
+        logs: perBrowserInfo[browser.id].logs
       });
     }
     perBrowserInfo[browser.id] = {
       currentTest: undefined,
-      logs: [],
+      logs: []
     };
     perBrowserInfo[browser.id].logs = [];
   };
 
-  emitter.on('browser_info', (browser, info) => {
+  emitter.on('browser_info', function(browser, info) {
     if (info.current_spec) {
       perBrowserInfo[browser.id] = {
         currentTest: info.current_spec,
-        logs: [],
+        logs: []
       };
     }
   });
@@ -52,41 +70,44 @@ var SpecReporter = function (baseReporterDecorator, config, emitter) {
     var browserInfo = perBrowserInfo[browser.id];
 
     if (browserInfo === undefined) {
-      return
+      return;
     }
 
-    var currentTest = browserInfo.currentTest
+    var currentTest = browserInfo.currentTest;
 
     if (currentTest === undefined) {
       return;
     }
 
-    this.writeCommonMsg('\n' + browser.name + ' Had error during execution of test: ' + formatTest(currentTest) + '\n');
+    this.writeCommonMsg(
+      '\n' + browser.name + ' Had error during execution of test: ' + formatTest(currentTest) + '\n'
+    );
 
     this.writeCommonMsg('Captured the following logs \n');
 
-
-    var output = browserInfo.logs.map((log) => {
-      return log.type + ': ' + log.log;
-    }).join('\n');
+    var output = browserInfo.logs
+      .map(function(log) {
+        this.writeCommonMsg(formatLog(log.type, log.log));
+      })
+      .join('\n');
 
     this.writeCommonMsg('\n' + travisFold.wrap('output', output) + '\n');
-  }
+  };
 
   this.onRunComplete = function() {
     perBrowserInfo = {};
     var output = [];
-    failures.forEach(function (failure) {
+    failures.forEach(function(failure) {
       travisFold.pushStart(output, 'browser');
       output.push('Logs for ' + failure.browser.name + ' ' + failure.specResult.fullName);
-      failure.logs.forEach(function (log) {
-        output.push(log.type + ': ' + log.log);
+      failure.logs.forEach(function(log) {
+        output.push(formatLog(log.type, log.log));
       });
       travisFold.pushEnd(output, 'browser');
     });
     this.writeCommonMsg('\n' + output.join('\n') + '\n');
     failures = [];
-  }
+  };
 
   this.onBrowserLog = function(browser, log, type) {
     if (!perBrowserInfo[browser.id]) {
@@ -96,7 +117,7 @@ var SpecReporter = function (baseReporterDecorator, config, emitter) {
     if (perBrowserInfo[browser.id].logs.length > 1000) {
       perBrowserInfo[browser.id].logs.shift();
     }
-  }
+  };
 };
 
 SpecReporter.$inject = ['baseReporterDecorator', 'config', 'emitter'];
